0571bf56bc092c8e96840fff658bd5911cd6f1a8
brent <jinhoshin319@gmail.com>
2021-12-22 21:42:33 +0900

    portfolio related file initial commit

---
+++ app/src/main/java/com/projectseoul/stockmarkettest/screens/FragmentPortfolio.kt
@@ -0,0 +1,21 @@
+package com.projectseoul.stockmarkettest.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by KING JINHO on 12/21/2021
+ */
+class FragmentPortfolio : BaseBottomFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/projectseoul/stockmarkettest/viewmodels/FragmentPortfolioViewModel.kt
@@ -0,0 +1,13 @@
+package com.projectseoul.stockmarkettest.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+/**
+ * Created by KING JINHO on 12/22/2021
+ */
+class FragmentPortfolioViewModel(application: Application) : AndroidViewModel(application) {
+
+
+
+}
