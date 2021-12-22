ddf6f80c071490bd2891ed6265694578b7602cb5
brent <jinhoshin319@gmail.com>
2021-12-22 21:43:58 +0900

    calculate drawdown test case

--- app/src/test/java/com/projectseoul/stockmarkettest/ExampleUnitTest.kt
+++ app/src/test/java/com/projectseoul/stockmarkettest/ExampleUnitTest.kt
@@ -1,8 +1,14 @@
 package com.projectseoul.stockmarkettest
 
+import com.projectseoul.stockmarkettest.models.Drawdown
+import com.projectseoul.stockmarkettest.models.Quote
 import org.junit.Test
+import com.google.common.truth.Truth.assertThat
+import com.projectseoul.stockmarkettest.TestData.quotations
 
-import org.junit.Assert.*
+
+import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +16,99 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @Test
+    fun `empty list returns false`() {
+        assertThat(quotations.isEmpty()).isFalse()
+    }
+
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        val list = findDrawdown()
+        assertThat(list.size).isEqualTo(7)
+
     }
+
+
+    private fun findDrawdown(): List<Drawdown> {
+        val list = mutableListOf<Drawdown>()
+        var peak: Quote? = null
+        var trough: Quote? = null
+        var recovered: Quote? = null
+
+        for (quotation in quotations) {
+            val currentIdx = quotations.indexOf(quotation)
+            if (currentIdx in 1 until quotations.size - 1) {
+                val prevClosing = quotations[currentIdx - 1].price
+                val nextClosing = quotations[currentIdx + 1].price
+                val closing = quotation.price
+
+                recovered = when {
+                    peak == null -> null
+                    closing >= peak.price -> {
+                        if (peak.date != quotation.date) {
+                            //can possibly be new peak
+                            Quote(closing, quotation.date)
+                        } else null
+                    }
+                    else -> null
+                }
+
+                peak = if (closing > prevClosing && closing > nextClosing) {
+                    if ((peak == null || peak.price < closing) && recovered == null) {
+                        Quote(closing, quotation.date)
+                    } else peak
+                } else peak
+
+                trough = if (closing < prevClosing && closing < nextClosing) {
+                    if (trough == null || trough.price > closing) {
+                        Quote(closing, quotation.date)
+                    } else trough
+                } else trough
+
+
+                if (recovered != null) {
+                    val drawdown = Drawdown()
+                    val percentage = (peak!!.price - trough!!.price) / peak.price
+                    if(percentage > 0.1) {
+                        drawdown.peak = peak
+                        drawdown.trough = trough
+                        drawdown.recovered = recovered
+                        drawdown.percentage = percentage
+                        drawdown.daysToRecover =
+                            ChronoUnit.DAYS.between(
+                                LocalDate.of(
+                                    peak.date.substring(0, 4).toInt(),
+                                    peak.date.substring(4, 6).toInt(),
+                                    peak.date.substring(6, 8).toInt()
+                                ),
+                                LocalDate.of(
+                                    recovered.date.substring(0, 4).toInt(),
+                                    recovered.date.substring(4, 6).toInt(),
+                                    recovered.date.substring(6, 8).toInt()
+                                ).plusDays(1)
+                            ).toString()
+                        list += drawdown
+                    }
+                    peak = if (closing > prevClosing && closing > nextClosing) {
+                        Quote(recovered.price, recovered.date)
+                    } else {
+                        null
+                    }
+                    trough = null
+                    recovered = null
+                }
+            }
+        }
+        val drawdown = Drawdown()
+        val percentage = (peak!!.price - trough!!.price) / peak.price
+
+        drawdown.peak = peak
+        drawdown.trough = trough
+        drawdown.recovered = recovered
+        drawdown.percentage = percentage
+        list += drawdown
+        return list
+    }
+
 }
