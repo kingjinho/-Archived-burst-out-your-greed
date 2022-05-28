093a04a3e0b8d8bbf18c009ff678cba5dfce479d
brent <jinhoshin319@gmail.com>
2022-05-28 09:57:27 +0900

    create it to tell an application use hilt

---
+++ app/src/main/java/com/projectseoul/stockmarkettest/StockMarketApplication.kt
@@ -0,0 +1,11 @@
+package com.projectseoul.stockmarkettest
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by KING JINHO on 5/28/2022
+ */
+@HiltAndroidApp
+class StockMarketApplication : Application() {
+}
