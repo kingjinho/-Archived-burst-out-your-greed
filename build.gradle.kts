8841ed5583db79b00c8dad580dd013b359d8eeda
kingjinho <jinhoshin319@gmail.com>
2023-07-02 19:50:23 +0900

    register task delete

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,16 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:7.3.1'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10"
-        classpath 'androidx.navigation:navigation-safe-args-gradle-plugin:2.5.2'
-        classpath 'com.google.dagger:hilt-android-gradle-plugin:2.44'
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.2")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
-
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks {
+    register("delete", Delete::class.java) {
+        delete(rootProject.buildDir)
+    }
 }
