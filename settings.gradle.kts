b5d66008e79ab635d00c43206f36642687858f6c
kingjinho <jinhoshin319@gmail.com>
2023-07-02 19:05:12 +0900

    module added

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,38 @@
+import java.net.URI
+
+include(":shared")
+
+
+include(":data:model")
+
+
+include(":data")
+
+
+include(":domain")
+
+
+
+include(":benchmark")
+
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url 'https://jitpack.io' }
+        maven {
+            url = URI("https://jitpack.io")
+        }
     }
 }
 rootProject.name = "StockMarketTest"
-include ':app'
+include(":app")
