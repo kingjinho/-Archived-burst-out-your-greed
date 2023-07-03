070587416dca4258ca5cacc24ab9ccfe42c1faf8
kingjinho <jinhoshin319@gmail.com>
2023-07-03 20:48:30 +0900

    change due to rename IAppComponent

--- app/src/main/java/com/projectseoul/stockmarkettest/StockMarketApplication.kt
+++ app/src/main/java/com/projectseoul/stockmarkettest/StockMarketApplication.kt
@@ -1,18 +1,18 @@
 package com.projectseoul.stockmarkettest
 
 import android.app.Application
-import com.projectseoul.stockmarkettest.di.application.AppModule
-import com.projectseoul.stockmarkettest.di.application.DaggerIAppComponent
-import com.projectseoul.stockmarkettest.di.application.IAppComponent
+import com.projectseoul.stockmarkettest.di.application.ApplicationModule
+import com.projectseoul.stockmarkettest.di.application.DaggerIApplicationComponent
+import com.projectseoul.stockmarkettest.di.application.IApplicationComponent
 
 /**
  * Created by KING JINHO on 5/28/2022
  */
 class StockMarketApplication : Application() {
 
-    val appComponent: IAppComponent by lazy {
-        DaggerIAppComponent.builder()
-            .appModule(AppModule(this))
+    val appComponent: IApplicationComponent by lazy {
+        DaggerIApplicationComponent.builder()
+            .applicationModule(ApplicationModule(this))
             .build()
     }
 
