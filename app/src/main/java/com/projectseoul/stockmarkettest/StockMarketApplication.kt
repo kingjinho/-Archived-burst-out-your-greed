04456b1439d0bea5828d12620fce2c5fd4c6762a
kingjinho <jinhoshin319@gmail.com>
2023-07-03 16:58:27 +0900

    add appComponent dagger component

--- app/src/main/java/com/projectseoul/stockmarkettest/StockMarketApplication.kt
+++ app/src/main/java/com/projectseoul/stockmarkettest/StockMarketApplication.kt
@@ -1,9 +1,19 @@
 package com.projectseoul.stockmarkettest
 
 import android.app.Application
+import com.projectseoul.stockmarkettest.di.application.AppModule
+import com.projectseoul.stockmarkettest.di.application.DaggerIAppComponent
+import com.projectseoul.stockmarkettest.di.application.IAppComponent
 
 /**
  * Created by KING JINHO on 5/28/2022
  */
 class StockMarketApplication : Application() {
+
+    val appComponent: IAppComponent by lazy {
+        DaggerIAppComponent.builder()
+            .appModule(AppModule(this))
+            .build()
+    }
+
 }
