df38e15fd3efad4956d1aedc664b510e8a824fd7
kingjinho <jinhoshin319@gmail.com>
2023-07-03 21:14:19 +0900

    version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("com.android.tools.build:gradle:8.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.2")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
