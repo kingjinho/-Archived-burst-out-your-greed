47f7656b80bfc494c19756e39f760ffd6e96b7c3
kingjinho <jinhoshin319@gmail.com>
2023-07-04 19:21:36 +0900

    add inject() for fragments

--- app/src/main/java/com/projectseoul/stockmarkettest/di/activity/IActivityComponent.kt
+++ app/src/main/java/com/projectseoul/stockmarkettest/di/activity/IActivityComponent.kt
@@ -1,6 +1,9 @@
 package com.projectseoul.stockmarkettest.di.activity
 
 import com.projectseoul.stockmarkettest.di.annotation.scope.ActivityScope
+import com.projectseoul.stockmarkettest.screens.detail.ScreenStockDetail
+import com.projectseoul.stockmarkettest.screens.main.ScreenMain
+import com.projectseoul.stockmarkettest.screens.splash.ScreenSplash
 import dagger.Component
 
 @ActivityScope
@@ -9,4 +12,7 @@ import dagger.Component
 )
 interface IActivityComponent {
 
+    fun inject(screen: ScreenMain)
+    fun inject(screen: ScreenStockDetail)
+    fun inject(screen: ScreenSplash)
 }
