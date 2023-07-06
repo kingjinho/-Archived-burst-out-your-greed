77120b5ecd6ee5a49683d25abd6e1e13eeba8e22
kingjinho <jinhoshin319@gmail.com>
2023-07-06 14:51:05 +0900

    refactor: add Factory interface

--- app/src/main/java/com/projectseoul/stockmarkettest/di/activity/IActivityComponent.kt
+++ app/src/main/java/com/projectseoul/stockmarkettest/di/activity/IActivityComponent.kt
@@ -4,13 +4,18 @@ import com.projectseoul.stockmarkettest.di.annotation.scope.ActivityScope
 import com.projectseoul.stockmarkettest.screens.detail.ScreenStockDetail
 import com.projectseoul.stockmarkettest.screens.main.ScreenMain
 import com.projectseoul.stockmarkettest.screens.splash.ScreenSplash
-import dagger.Component
+import dagger.Subcomponent
 
 @ActivityScope
-@Component(
+@Subcomponent(
     modules = [ActivityModule::class]
 )
 interface IActivityComponent {
+
+    @Subcomponent.Factory
+    interface Factory {
+        fun create(): IActivityComponent
+    }
 
     fun inject(screen: ScreenMain)
     fun inject(screen: ScreenStockDetail)
